f43d9aeb902a8d93da0df7823e95af9e4b82a2ed
hujikol <nikolasnanda@gmail.com>
2026-03-06 22:30:20 +0700

    new zauth vector token

--- src/app/.well-known/vector-verify/route.ts
+++ src/app/.well-known/vector-verify/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    token: "aa6d67a69090602e622fd0344f44bd7d56a0429e4f73b427b07bdd8b8c3e86be",
+    token: "6e80470500c9bfd094decdb98a3853f2d064faa5cb4aa8494d99b0eae6952b09",
   });
 }
