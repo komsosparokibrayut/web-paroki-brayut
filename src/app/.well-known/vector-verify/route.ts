36e6d9f8e56386996bec191239466431a242f857
hujikol <nikolasnanda@gmail.com>
2026-03-06 23:05:11 +0700

    removing zauth vector verification

--- src/app/.well-known/vector-verify/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    token: "6e80470500c9bfd094decdb98a3853f2d064faa5cb4aa8494d99b0eae6952b09",
-  });
-}
