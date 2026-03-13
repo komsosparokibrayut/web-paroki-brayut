329a5e956c6f92c5003cec4f05ef630524a88a27
hujikol <nikolasnanda@gmail.com>
2026-03-13 09:19:44 +0700

    fix: artikel 500 error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: [
+    "isomorphic-dompurify",
+    "jsdom",
+    "cssstyle",
+    "@asamuzakjp/css-color",
+    "@csstools/css-calc",
+  ],
   images: {
     remotePatterns: [
       {
